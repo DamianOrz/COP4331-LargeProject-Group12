afa14075396e2bd25d2c4917975d35ce6bfa2811
DamianOrz <Damian.21.10.02@gmail.com>
2026-07-06 15:17:32 -0400

    Automatically update droplet when pushing to main

---
+++ createJWT.js
@@ -0,0 +1,44 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+exports.createToken = function ( fn, ln, id )
+{
+    return _createToken( fn, ln, id );
+}
+
+_createToken = function ( fn, ln, id )
+{
+    try
+    {
+        const user = {userId:id,firstName:fn,lastName:ln};
+        const accessToken = jwt.sign( user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '30m' });
+        var ret = {accessToken:accessToken};
+    }
+    catch(e)
+    {
+        var ret = {error:e.message};
+    }
+    return ret;
+}
+
+exports.isExpired = function( token )
+{
+    try
+    {
+        jwt.verify( token, process.env.ACCESS_TOKEN_SECRET );
+        return false; // token is valid
+    }
+    catch(e)
+    {
+        return true; // token is invalid or expired
+    }
+}
+
+exports.refresh = function( token )
+{
+    var ud = jwt.decode(token,{complete:true});
+    var userId = ud.payload.userId;
+    var firstName = ud.payload.firstName;
+    var lastName = ud.payload.lastName;
+    return _createToken( firstName, lastName, userId );
+}
