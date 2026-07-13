7bc806ebc884b64d3a5b31a350ac21a78279b11c
Zaki Ahamad <48259940+ZakiAhamad@users.noreply.github.com>
2026-07-13 18:42:53 -0400

    Revert "API"
    This reverts commit 027bca599f4e56d9a176135fe98a6bff0b499f6d.

--- backend/schema/card.ts
+++
@@ -1,8 +0,0 @@
-import mongoose, { Schema } from 'mongoose';
-
-const CardSchema: Schema = new Schema({
-    Card: { type: String, required: true },
-    UserId: { type: String, required: true }
-});
-
-export default mongoose.model('Card', CardSchema, 'Cards');
